a7b35c6fc7ad20326a8e635166de79d7735e1f8b
ristuo <risto.tuomainen@helsinki.fi>
2015-01-22 18:27:39 +0200

    hieman parannusta templateen

--- app/views/testi.php
+++ app/views/testi.php
@@ -10,6 +10,22 @@
 	<body>
 		
 		<div>asdfasdfsaafdsaf adsfasd jotain jotain</div>
-		<table table-striped>
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<td><strong>Aika</strong></td>
+					<td>Matti</td>
+					<td>Maija</td>
+					
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td> Maanantai 10-12 </td>
+					<td = class="success"></td>
+					<td = class="danger"></td>
+				</tr>
+			</tbody>
+		</table>
 	</body>
 </html>
