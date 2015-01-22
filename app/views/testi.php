74b5f809da95288bd3a82019283dc1e02a6ffa7a
ristuo <risto.tuomainen@helsinki.fi>
2015-01-22 18:15:08 +0200

    htmptemplate

---
+++ app/views/testi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>html-template</title>
+		<link href="bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+		<link href="bootstrap-3.3.2-dist/css/bootstrap-theme.css" rel="stylesheet">
+		<link href="bootstrap-3.3.2-dist/css/main.css" rel="stylesheet">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
+		
+		<div>asdfasdfsaafdsaf adsfasd jotain jotain</div>
+		<table table-striped>
+	</body>
+</html>
