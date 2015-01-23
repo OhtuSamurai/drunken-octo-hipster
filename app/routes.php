3dff683654d2fcf2d7f51fb9326bb166b5ad1fad
mihassin <marko.hassinen@helsinki.fi>
2015-01-23 17:09:35 +0200

    app/view/testi.php:lle 'testi'

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,10 @@
 
 Route::get('/', function()
 {
+	return View::make('hello');
+});
+
+Route::get('testi', function()
+{
 	return View::make('testi');
 });
---
+++ app/tests/TestiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class TestiTest extends TestCase {
+
+	/**
+   * Author: mihassin
+	 * Eka testi template-view:iä varten
+	 *
+	 * @return void
+	 */
+	public function testTestissaLukeeAika()
+	{
+		$crawler = $this->client->request('GET', 'testi');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+
+    $this->assertCount(1, $crawler->filter('body:contains("Aika")'));
+	}
+
+}
