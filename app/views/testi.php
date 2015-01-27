f1ff671d99ff0f333446497ccdeb184e1e27188b
mihassin <marko.hassinen@helsinki.fi>
2015-01-27 18:24:49 +0200

    mergeys masteriin

--- app/views/testi.php
+++ app/views/testi.php
@@ -1,7 +1,15 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="container">
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>html-template</title>
+		<link href="bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+		<link href="bootstrap-3.3.2-dist/css/bootstrap-theme.css" rel="stylesheet">
+		<link href="bootstrap-3.3.2-dist/css/main.css" rel="stylesheet">
+		<link href="main.css" rel="stylesheet">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
+		<div class="container">		
 		<h1>Sopivat ajat opetustaitotoimikunnan tapaamiseen</h1>
 		<table class="table table-bordered">
 			<thead>
@@ -12,7 +20,7 @@
 					<th class="col-md-1">Mikki</th>
 					<th class="col-md-1">Matti</th>
 					<th class="col-md-1">Tapio</th>
-					<th class="col-md-1">Liisa</th>
+					<th class="col-md-1">Liisa</th>	
 					<th class="col-md-1">Tulos</th>
 				</tr>
 			</thead>
@@ -50,4 +58,5 @@
 			</tbody>
 		</table>
 		</div>
-@stop
+	</body>
+</html>
