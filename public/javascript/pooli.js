11afb2cc3da4a97c31f9889566a944ad02c8dcc1
Johanna <johanna.keto@helsinki.fi>
2015-02-01 18:41:37 +0200

    rivit muuttuvat siniseksi kun klikkaa, valkoisiksi kun klikkaa uudestaan

--- public/javascript/pooli.js
+++ public/javascript/pooli.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $(".pooltable>tbody>tr").click(function(){
-        $(this).hide();
-    });
+  $(".pooltable>tbody>tr").click(function(){
+    checkbox = $(this).find(".userselector")
+     
+    if(checkbox.prop("checked")){
+      $(this).addClass("active");
+    }else{
+      $(this).removeClass("active");
+    }
+    checkbox.prop("checked", !checkbox.prop("checked"));	
+  });
 });
+
