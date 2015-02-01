b255060da7b8bd5b840f5e31f1a5d763bbb02dc1
Johanna <johanna.keto@helsinki.fi>
2015-02-01 17:17:20 +0200

    @yield poolin javascriptiä varten ja jquery lisätty

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,6 +7,8 @@
   {{HTML::style('bootstrap-3.3.2-dist/css/bootstrap-theme.min.css')}}
   {{HTML::style('main.css')}}
   {{HTML::script('bootstrap-3.3.2-dist/js/bootstrap.min.js')}}
+  {{HTML::script('javascript/jquery-1.11.2.min.js')}}
+  @yield('pagehead')
 </head>
 <body>
 @include('layouts.navigation')
