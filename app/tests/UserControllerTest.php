5538ad7416ffd9d08b6bcd742a95c51579ca33e4
mihassin <marko.hassinen@helsinki.fi>
2015-02-09 12:03:12 +0200

    testaus kesken

--- app/tests/UserControllerTest.php
+++ app/tests/UserControllerTest.php
@@ -2,17 +2,12 @@
 
 class UserControllerTest extends TestCase {
 
-	public function getIndex() {
-		Event::all();
-		return "moi";
-	}
-
 	public function testIndex() {
-		Event::shouldReceive('all')
+		/*Event::shouldReceive('all')
 			 ->once();
 
 		$this->call('GET', 'pooli');
 
-		$this->assertViewHas('users');
+		$this->assertViewHas('users');*/
 	}
 }
