3467b7a9d0c27a4cdb982fd897c442fda07c4151
mihassin <marko.hassinen@helsinki.fi>
2015-02-10 21:29:30 +0200

    merge

--- app/tests/OtsikkoTest.php
+++ app/tests/OtsikkoTest.php
@@ -2,21 +2,13 @@
 class OtsikkoTest extends PHPUnit_Extensions_Selenium2TestCase
 {
 
-    protected $webDriver;
-
     protected function setUp()
     {
-<<<<<<< HEAD
         $this->setHost('localhost');
         $this->setPort(4444);
         $this->setBrowser('htmlunit');
         //$this->setBrowser('firefox');
         $this->setBrowserUrl('http://homestead.app/');   
-=======
-        //$this->setBrowser('htmlunit');
-        $this->setBrowser('firefox');
-        $this->setBrowserUrl('http://localhost');
->>>>>>> 503388a7601b8ce1ae2c8d2124a610755b95a22e
     }
 
     public function testTitle()
