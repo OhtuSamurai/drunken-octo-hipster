93c9a14790685bfdda602ecc7109901d70c46a81
ristuo <risto.tuomainen@helsinki.fi>
2015-02-12 15:34:09 +0200

    a

--- app/tests/controllers/TimeideaControllerTest.php
+++ app/tests/controllers/TimeideaControllerTest.php
@@ -40,4 +40,20 @@ class TimeideaControllerTest extends TestCase {
 		$a = new TimeideaController;
 		$this->assertNull($a->destroy(1));
 	}
+	
+	public function testStore() {
+		$tic = new TimeideaController;
+		Request::replace($input=['poll_id'=>'25','begins'=>'14:00','ends'=>'16:00','date'=>'2015-11-11']);
+		$polli = $this->mockPoll(25);
+		$polli->save();
+		$u = $this->mockUser(25);
+		$u->save();
+		$polli->users()->attach($u);
+		$tic->Store();
+		$this->assertTrue(Timeidea::find(1)->date=='2015-11-11');	
+		$this->assertTrue(Timeidea::find(1)->begins=='14:00');
+		$vastaukset =  $polli->answers();
+		foreach($vastaukset as $vastaus)
+			$this->assertTrue($vastaus->sopivuus=='eisovi');
+	}
 }
