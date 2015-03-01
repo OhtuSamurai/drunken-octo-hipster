7cc28a7d984ba46eecfa79a2bfb90c473dd7a835
mihassin <marko.hassinen@helsinki.fi>
2015-03-01 21:23:26 +0200

    committee.show näkymä

--- app/views/committee/show.blade.php
+++ app/views/committee/show.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.master')
 
 @section('content')
-{{ $committee->name }}
+	<h1>{{ $committee->name }}</h1>
+	<h2>Järjestetään: {{ $committee->time }}</h2>
+
+    <table class="pooltable table table-hover">
+  	<thead>
+    	<tr>
+      	<th>Etunimi</th>
+      	<th>Sukunimi</th>
+      	<th>Laitos</th>
+      	<th>Asema</th>
+    	</tr>
+  	</thead>
+ 	<tbody>
+ 	@foreach($users as $user)
+    <tr data-userid="{{$user->id}}" >
+      <td>{{$user->first_name}}</td>
+      <td>{{$user->last_name}}</td>
+      <td>{{$user->department}}</td>
+      <td>{{$user->position}}</td>
+    </tr>
+
+  	@endforeach
+  	</tbody>
+	</table>
+
 @stop
