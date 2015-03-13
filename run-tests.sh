6f72e68b84af1715b86ce93caaf2b5f1d4441455
vagrant <vagrant@homestead>
2015-03-13 09:43:26 +0000

    testien ajaminen korjattu, eka coverage yritys

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #author kumikumi
 command -v phantomjs >/dev/null 2>&1 || { echo "Asennetaan phantomjs. Hetki"; sudo apt-get update >/dev/null && sudo apt-get install --yes phantomjs; }
-phantomjs --webdriver=4444 & vendor/bin/codecept run
+phantomjs --webdriver=4444 & vendor/bin/codecept run --coverage --xml --html
 killall phantomjs
