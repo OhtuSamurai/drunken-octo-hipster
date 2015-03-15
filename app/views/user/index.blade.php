9d21fb11da70c5ffecb63d45f7518b24a0295e7b
ristuo <risto.tuomainen@helsinki.fi>
2015-03-15 22:12:08 +0200

    muokkasin viewssejä niin että taulukot ei ole ylenmääräisen leveitä

--- app/views/user/index.blade.php
+++ app/views/user/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="col-md-7">
   <h1>Opetustaitotoimikuntapooli</h1>
 
   @include('user._form')
+</div>
 @stop
--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <h1>Yhteenveto</h1>
 
-<div class="col-md-6">
+<div class="col-md-4">
   <h2>Päätetyt toimikunnat:</h2>
 	@include('committee.list')
 </div>
-<div class="col-md-6">
+<div class="col-md-4">
   <h2>Avoimet kyselyt:</h2>
 	@include('poll.list')
 </div>
