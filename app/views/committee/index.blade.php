896129159be217c5ebb8ab7d3bd862f26ae29a0d
ristuo <risto.tuomainen@helsinki.fi>
2015-03-29 18:22:19 +0300

    button-bugin korjaus

--- app/views/committee/index.blade.php
+++ app/views/committee/index.blade.php
@@ -8,10 +8,10 @@
 
 @if(Auth::user() && Auth::user()->is_admin)
 	<div class ="row">
-		<div class="col-md-6">
+		<div class="top7">
   			<a href="/poll/create" class="btn btn-primary" role="button">Luo uusi toimikunta kyselyn kautta</a>
 		</div>
-		<div class="col-md-6">
+		<div class="top7">
   <a href="/committee/create" class="btn btn-primary" role="button">Luo uusi toimikunta ilman kyselyä</a>
 		</div>
 	</div>
