e149a3a5f898001f54bcb34bf6eeb38875729321
Johanna <johanna.keto@helsinki.fi>
2015-04-08 11:47:49 +0300

    userlist oletuksena järkässä laitoksen mukaan

--- app/views/user/list.blade.php
+++ app/views/user/list.blade.php
@@ -1,18 +1,20 @@
 @if(Auth::user())
+
 <table class="pooltable table table-hover">
   <thead>
     <tr>
-      <th>Etunimi</th>
-      <th>Sukunimi</th>
-      <th>Sähköposti</th>
-      <th>Laitos</th>
-      <th>Asema</th>
+      <th class = "firstname">Etunimi</th>
+      <th class = "lastname">Sukunimi</th>
+      <th class = "email">Sähköposti</th>
+      <th class = "department">Laitos</th>
+      <th class = "position">Asema</th>
     </tr>
   </thead>
   <tbody>
+  
   @foreach($users as $user)
 
-    <tr data-userid="{{$user->id}}" >
+    <tr class="rivit" data-userid="{{$user->id}}" >
       <td><a href="/user/{{$user->id}}">{{$user->first_name}}</a><input class="userselector hidden" type="checkbox" name="user[]" value="{{$user->id}}"></td>
       <td>{{$user->last_name}}</td>
       <td>{{$user->email}}</td>
--- public/javascript/pooli.js
+++ public/javascript/pooli.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
   $(".userselector").prop("checked", false);
-	
+
+	sortlist($(".department"));	//oletusjärkkä laitoksen mukaan
+
+	function sortlist(bythis){
+    	var table = bythis.parents('table').eq(0)
+    	var rows = table.find('tr:gt(0)').toArray().sort(comparer(bythis.index()))
+    	this.asc = !this.asc
+    	if (!this.asc){rows = rows.reverse()}
+    	for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+	}
+
+	function comparer(index) {
+	    return function(a, b) {
+	    	var valA = getCellValue(a, index), valB = getCellValue(b, index)
+	  	    return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+	    	}
+	}
+	function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
+
+
   $(".pooltable>tbody>tr").click(function(){
     checkbox = $(this).find(".userselector")
      
