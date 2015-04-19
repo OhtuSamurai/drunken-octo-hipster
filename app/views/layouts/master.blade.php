9ef6f513101983f21d44f4739d57081ac1d6543b
ristuo <risto.tuomainen@helsinki.fi>
2015-04-19 19:48:37 +0300

    eiku toi äskeinen olikin ruma, tein uudestaan

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -35,7 +35,11 @@
 
 @if (Auth::user() && !Auth::user()->is_admin && count(Auth::user()->unansweredpolls())>0)
 	<div class="alert alert-warning">
-		<p>Sinulla on vastaamattomia kyselyjä!</p>
+		<p>Sinulla on vastaamattomia kyselyjä! Siirry vastaamaan linkistä:
+		@foreach (array_unique(Auth::user()->unansweredpolls()) as $uapolli_id)
+			<a href="/poll/{{$uapolli_id}}">{{Poll::find($uapolli_id)->toimikunta}}</a>
+		@endforeach
+</p>
 	</div>
 @endif
 
