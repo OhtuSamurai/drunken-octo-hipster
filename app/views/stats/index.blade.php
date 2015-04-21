087f2b05f0ff9dc9d1a003a4e04e3080d16be1cf
ristuo <risto.tuomainen@helsinki.fi>
2015-04-21 19:33:03 +0300

    kivasti riviin ne ylimmät

--- app/views/stats/index.blade.php
+++ app/views/stats/index.blade.php
@@ -1,56 +1,68 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="col-md-7">
 <h1> Yleisiä tilastoja</h1>
+<div class="row">
+<div class="col-md-3">
 <h3>Käyttäjät</h3>
 	<table class="table table-striped">
 		<tr>
 			<td>Käyttäjiä</td>
 			<td>{{$tilasto->n_users}}</td>
-			<td></td>
+		</tr>
+		<tr>
 			<td>Käyttäjiä toimikunnissa</td>
 			<td>{{$tilasto->n_komiteoissa}}</td>
 		</tr>
 		<tr>
 			<td>Käyttäjiä poolissa</td>
 			<td>{{$tilasto->n_pooli}}</td>
-			<td></td>
+		</tr>
+		<tr>
 			<td>Käyttäjiä kyselyissä</td>
 			<td>{{$tilasto->n_kyselyissa}}</td>
 		</tr>
 	</table>
+</div>
+<div class="col-md-3">
 <h3>Toimikunnat ja kyselyt</h3>
 	<table class="table table-striped">
 		<tr>
 			<td>Kyselyjä</td>
 			<td>{{$tilasto->n_polls}}</td>
-			<td></td>
+		</tr>
+		<tr>
 			<td>Avoimia kyselyjä</td>
 			<td>{{$tilasto->n_openpolls}}</td>
 		</tr>
 		<tr> 
 			<td>Toimikuntia</td>
 			<td>{{$tilasto->n_committees}}</td>
-			<td></td>
+		</tr>
+		<tr>
 			<td>Avoimia toimikuntia</td>
 			<td>{{$tilasto->n_opencommittees}}</td>
 		</tr>
 	</table>
-
+</div>
+<div class="col-md-3">
 <h3>Liitteet</h3>
 	<table class="table table-striped">
 		<tr>
 			<td>Liitteitä</td>
 			<td>{{$tilasto->n_attachments}}</td>
-			<td></td>
-			<td>Liiitteiden yhteenlaskettu koko</td>
+		</tr>
+		<tr>
+			<td>Liiitteiden yhteenlaskettu koko (MB)</td>
 			<td>{{$tilasto->attachmentsize}}</td>
 		</tr>
 	</table>
 </div>
-<div class="col-md-7">
+</div>
+<div class="row">
+<div class="col-md-9">
 <h1>Käyttäjäkohtaisia tilastoja</h1>
 @include('user.listwithdate')
-
+</div>
+</div>
 @stop
